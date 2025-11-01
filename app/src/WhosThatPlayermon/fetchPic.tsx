765cfa20fbe5ae5e9913f4efccefe1ee58ca4c73
Daniel Cepeda <dcep93@gmail.com>
2025-10-31 22:39:13 -0400

    x

--- app/src/WhosThatPlayermon/fetchPic.tsx
+++ app/src/WhosThatPlayermon/fetchPic.tsx
@@ -6,5 +6,16 @@ export type PicData = {
 };
 
 export default function fetchPic(user: User): Promise<PicData> {
-  return Promise.resolve({ answer: "answer", src: "src" });
+  return fetch("https://api.openai.com/v1/images/generations", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.oauth_token}`,
+    },
+    body: JSON.stringify({
+      prompt:
+        "Find a published action shot of a random football player during the 2025 season. This will be used for a quiz, so if their name is visible on their jersey, blur it out. I need to know the player's name too.",
+      size: "512x512",
+    }),
+  }).then((resp) => resp.json());
 }
--- app/src/WhosThatPlayermon/useAuth.tsx
+++ app/src/WhosThatPlayermon/useAuth.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
-export type User = { email: string; key: string };
+export type User = { email: string; oauth_token: string };
 
 export default function useAuth() {
   const [user, setUser] = useState<User | null>(null);
   return [
     user,
-    () => setUser(user ? null : { key: "key", email: Date.now().toString() }),
+    () =>
+      setUser(
+        user
+          ? null
+          : { oauth_token: "oauth_token", email: Date.now().toString() }
+      ),
   ] as const;
 }
