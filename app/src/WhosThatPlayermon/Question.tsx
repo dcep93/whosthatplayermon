9e65fc49e89dd12ef348886d00f25edc80be4cfb
Daniel Cepeda <dcep93@gmail.com>
2025-11-03 14:18:19 -0500

    x

--- app/src/WhosThatPlayermon/Question.tsx
+++ app/src/WhosThatPlayermon/Question.tsx
@@ -4,7 +4,9 @@ import type { Entry } from "./Game";
 export default function Question(props: { entry: Entry }) {
   return (
     <Stack gap="sm">
-      <pre>{JSON.stringify(props.entry, null, 2)}</pre>
+      <pre>
+        {JSON.stringify({ ...props.entry, imgSrc: undefined }, null, 2)}
+      </pre>
       {props.entry.imgSrc ? (
         <img
           alt={props.entry.playerName}
